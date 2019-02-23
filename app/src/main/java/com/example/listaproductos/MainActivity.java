b975a8221ba353b75d2a0fa193b01b2ce70aacaf
Elias Lopez Cabrera <eliaslcabrera@gmail.com>
2019-02-22 21:04:04 -0700

    Agregar Productos

--- app/src/main/java/com/example/listaproductos/MainActivity.java
+++ app/src/main/java/com/example/listaproductos/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.listaproductos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch(item.getItemId()){
+            case R.id.mi_addProducts:
+                Intent intent = new Intent(getApplicationContext(),addProductos.class );
+                startActivity(intent);
+                return true;
+
+            default: return false;
+        }
     }
 }
